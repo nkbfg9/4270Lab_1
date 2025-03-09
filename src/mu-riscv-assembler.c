84a623b2cba3f75976fe9ce7030b97119683bfea
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2025-03-09 17:47:41 -0500

    Changed prog_tokens formated to 2D Array

--- src/mu-riscv-assembler.c
+++ src/mu-riscv-assembler.c
@@ -5,33 +5,43 @@
 #include <assert.h>
 
 char prog_file[32];
-char * prog_tokens[1000];
-int num_tokens = 0;
-const char * delimeter = " ";
-char * temp;
+char *prog_tokens[256][4];
+int num_lines = 0;
+const char *delimeter = " ";
+char *temp;
 
-void load_program() {
-    FILE * fp;
-	char * line = NULL;
+void load_program()
+{
+    FILE *fp;
+    char *line = NULL;
     size_t len = 0;
     size_t read;
 
-	/* Open program file. */
-	fp = fopen(prog_file, "r");
-	if (fp == NULL) {
-		printf("Error: Can't open program file %s\n", prog_file);
-		exit(-1);
-	}
+    /* Open program file. */
+    fp = fopen(prog_file, "r");
+    if (fp == NULL)
+    {
+        printf("Error: Can't open program file %s\n", prog_file);
+        exit(-1);
+    }
 
-	/* Read in the program. */
-    while ((read = getline(&line, &len, fp)) != -1) {
+    /* Read in the program. */
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        int tk = 0;
+        const char comment = '#';
         temp = strtok(line, delimeter);
-        while(temp != NULL) {
-            prog_tokens[num_tokens] = (char *) malloc(sizeof(char) * strlen(temp));
-            strcpy(prog_tokens[num_tokens], temp);
-            num_tokens++;
+        while (temp != NULL)
+        {
+            if (tk > 3 || strncmp(temp, &comment, 1) == 0)
+                break;
+
+            prog_tokens[num_lines][tk] = (char *)malloc(sizeof(char) * strlen(temp));
+            strcpy(prog_tokens[num_lines][tk], temp);
+            tk++;
             temp = strtok(NULL, delimeter);
         }
+        num_lines++;
     }
 
     fclose(fp);
@@ -40,23 +50,30 @@ void load_program() {
     return;
 }
 
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     printf("\n**************************\n");
-	printf("Welcome to MU-RISCV SIM...\n");
-	printf("**************************\n\n");
-	
-	if (argc < 2) {
-		printf("Error: You should provide input file.\nUsage: %s <input program> \n\n",  argv[0]);
-		exit(1);
-	}
+    printf("Welcome to MU-RISCV SIM...\n");
+    printf("**************************\n\n");
 
-    strcpy(prog_file, argv[1]);
-    load_program(prog_file);
-
-    for(int i = 0; i < num_tokens; i++) {
-        printf("%s ", prog_tokens[i]);
+    if (argc < 2)
+    {
+        printf("Error: You should provide input file.\nUsage: %s <input program> \n\n", argv[0]);
+        exit(1);
     }
 
+    strcpy(prog_file, argv[1]);
+    load_program();
+
+    for (int i = 0; i < num_lines; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (prog_tokens[i][j])
+            {
+                printf("%s ", prog_tokens[i][j]);
+            }
+        }
+        printf("\n");
+    }
 }
